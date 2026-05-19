6d67cc330c73545da18b759a98892cda2c6415ce
Nurbol <beysenbaynurboo@gmail.com>
2026-05-19 16:54:19 +0500

    Добавил passwordEncoder + JWT dependencies

--- car_marketplace/src/main/java/com/nurbol/car_marketplace/BeysenbayNurbolService/UserService.java
+++ car_marketplace/src/main/java/com/nurbol/car_marketplace/BeysenbayNurbolService/UserService.java
@@ -3,6 +3,7 @@ package com.nurbol.car_marketplace.BeysenbayNurbolService;
 import com.nurbol.car_marketplace.BeysenbayNurbolEntity.User;
 import com.nurbol.car_marketplace.BeysenbayNurbolRepository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,12 +13,18 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     public User createUser(User user) {
+
+        user.setPassword(
+                passwordEncoder.encode(user.getPassword())
+        );
+
         return userRepository.save(user);
     }
 }
